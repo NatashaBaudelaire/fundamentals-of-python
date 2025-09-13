5ae22210a8d796549e07fa06be6299065775a6fe
Natasha Baudelaire <wasafraidtoleaveyou@gmail.com>
2025-09-13 16:23:14 -0300

    Delete 01-algorithms-and-logic/conditionals directory

--- 01-algorithms-and-logic/conditionals/age.py
+++
@@ -1,8 +0,0 @@
-age = int(input("Enter your age: "))
-if age < 18:
-    print('You are an adult!')
-elif age < 0:
-    print('You are not born yet!')
-else:
-    print('You are a minor!')
-print('Age check complete!')
